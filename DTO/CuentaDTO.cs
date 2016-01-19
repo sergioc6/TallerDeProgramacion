53a6a46f83363f481d6a788e6e47d427ea232231
NahuelLeiva <educmack@gmail.com>
2016-01-19 20:37:51 -0300

    DTO y Persistencia
    Archivos DTO y de Persistencia para la base de datos

---
+++ DTO/CuentaDTO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class CuentaDTO
+    {
+        private string iNombre;
+        private string iID;
+        private string iDireccion;
+        private string iServicio;
+        private string iContraseña;
+
+
+        CuentaDTO(string pNombre, string pID, string pDireccion, string pServicio, string pContraseña)
+        {
+            this.iNombre = pNombre;
+            this.iID = pID;
+            this.iDireccion = pDireccion;
+            this.iServicio = pServicio;
+            this.iContraseña = pContraseña;
+        }
+
+        public string Nombre
+        {
+            get { return this.iNombre; }
+            set { this.iNombre = value; }
+        }
+
+        public string ID
+        {
+            get { return this.iID; }
+            set { this.iID = value; }
+        }
+
+        public string Direccion
+        {
+            get { return this.iDireccion; }
+            set { this.iDireccion = value; }
+        }
+
+        public string Servicio
+        {
+            get { return this.iServicio; }
+            set { this.iServicio = value; }
+        }
+
+        public string Contraseña
+        {
+            get { return this.iContraseña; }
+            set { this.iContraseña = value; }
+        }
+    }
+}
---
+++ DTO/EmailDTO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class EmailDTO
+    {
+        private string iIDCorreo;
+        private string iEstado;
+        private string iRemitente;
+        private string iDestinatario;
+        private string iAsunto;
+        private string iCuerpo;
+        private string iFecha;
+
+        EmailDTO(string pIDCorreo, string pEstado, string pRemitente, string pDestinatario, string pAsunto, string pCuerpo, string pFecha)
+        {
+            this.iIDCorreo = pIDCorreo;
+            this.iEstado = pEstado;
+            this.iRemitente = pRemitente;
+            this.iDestinatario = pDestinatario;
+            this.iAsunto = pAsunto;
+            this.iCuerpo = pCuerpo;
+            this.iFecha = pFecha;
+        }
+        public string IDCorreo
+        {
+            get { return this.iIDCorreo; }
+            set { this.iIDCorreo = value; }
+        }
+
+        public string Estado
+        {
+            get { return this.iEstado; }
+            set { this.iEstado = value; }
+        }
+
+        public string Remitente
+        {
+            get { return this.iRemitente; }
+            set { this.iRemitente = value; }
+        }
+
+        public string Destinatario
+        {
+            get { return this.iDestinatario; }
+            set { this.iDestinatario = value; }
+        }
+
+        public string Asunto
+        {
+            get { return this.iAsunto; }
+            set { this.iAsunto = value; }
+        }
+
+        public string Cuerpo
+        {
+            get { return this.iCuerpo; }
+            set { this.iCuerpo = value; }
+        }
+
+        public string Fecha
+        {
+            get { return this.iFecha; }
+            set { this.iFecha = value; }
+        }
+    }
+}
